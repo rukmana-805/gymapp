93a45cd6fc1e4db1cb38fa55fe2aebf24e8223ae
rukmana-805 <rukmanabehera805@gmail.com>
2025-09-17 13:22:10 +0530

    FitnessAdvice.jsx changed

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     baseURL : process.env.REACT_APP_API_URL, //http://localhost:3000/api
+    // baseURL : "http://localhost:3000/api",
     withCredentials : true,
     headers : {
         "Content-Type" : "application/json"

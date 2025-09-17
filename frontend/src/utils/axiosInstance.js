00cafe7e6608e843f34e4edc2e51cd2fe48ce1c4
rukmana-805 <rukmanabehera805@gmail.com>
2025-09-18 00:00:05 +0530

    added raw url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 
+// console.log("API URL:", process.env.REACT_APP_API_URL);
+
 const axiosInstance = axios.create({
-    baseURL : `${process.env.REACT_APP_API_URL}/api`, //http://localhost:3000/api
+    // baseURL : `${process.env.REACT_APP_API_URL}/api`,
     // baseURL : "http://localhost:3000/api",
+    baseURL : `https://mybackend-3lwb.onrender.com/api`,
     withCredentials : true,
     headers : {
         "Content-Type" : "application/json"
